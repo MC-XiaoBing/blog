52117dfbe38311ec7d59d16b579478e0a9b0ebc8
drelf <759984721@qq.com>
2018-03-04 16:57:05 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    $str=file_get_contents('https://cn.bing.com/HPImageArchive.aspx?idx=0&n=1');
+    $str=file_get_contents('http://cn.bing.com/HPImageArchive.aspx?idx=0&n=1');
 if (preg_match("/<url>(.+?)<\/url>/ies", $str, $matches)) {
     $imgurl='https://cn.bing.com'.$matches[1];
 }
