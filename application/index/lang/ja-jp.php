c519a4b08dd94090ff54d26acb9fe8d7846294cf
Drelf <759984721@qq.com>
2018-03-10 20:23:03 +0800

    blog

---
+++ application/index/lang/ja-jp.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Project: Catfish.
+ * Author: A.J
+ * Date: 2016/11/29
+ */
+return [
+    'Article list' => '文章のリスト',
+    'No search found' => '検索内容は見つかりませんでし',
+    'Search' => 'サーチ',
+    'Message content must be filled out' => 'メッセージの内容を記入する必要があります',
+    'The e-mail format is incorrect' => '電子メール形式が正しくありません',
+    'secrecy' => '秘密',
+    'male' => '男性',
+    'female' => '女性'
+];
---
+++ application/index/lang/ru-ru.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Project: Catfish.
+ * Author: A.J
+ * Date: 2016/11/28
+ */
+return [
+    'Article list' => 'список статей',
+    'No search found' => 'не  найти  поиска контента',
+    'Search' => 'поиск',
+    'Message content must be filled out' => 'содержание сообщений,  должны заполнить',
+    'The e-mail format is incorrect' => 'Формат электронной почты неверен',
+    'secrecy' => 'Конфиденциальность',
+    'male' => 'мужчина',
+    'female' => 'женщина'
+];
